534a9a36b6bc7a7e945953f17f5a91d8bc2676f2
Padwin <115787932+AFreeBaldy@users.noreply.github.com>
2023-03-03 08:22:20 -0500

    Initial commit

--- server/server.cpp
+++ server/server.cpp
@@ -2,4 +2,14 @@
 // Created by User on 3/2/2023.
 //
 
+#define SERVER_IP "127.0.0.1"
+void onPacketRecieved() {
 
+}
+
+void testServer() {
+    // Setup connection
+    TCP_Connection connection;
+    // Bind to a port
+    connection.bindTo(SERVER_IP, SERVER_PORT, onPacketRecieved);
+}
