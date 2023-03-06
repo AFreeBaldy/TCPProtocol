9f24fdf58adda20833b54351e8e96b99fcd486f5
Padwin <115787932+AFreeBaldy@users.noreply.github.com>
2023-03-05 22:05:10 -0500

    Added implementation for converting the raw binaries into a tcp packet struct

--- tcp/tcp_packet.h
+++ tcp/tcp_packet.h
@@ -5,17 +5,7 @@
 #ifndef TCPPROTOCOL_TCP_PACKET_H
 #define TCPPROTOCOL_TCP_PACKET_H
 
-struct TCP_packet {
-    unsigned short sourcePort;
-    unsigned short destinationPort;
-    unsigned int sequenceNumber;
-    unsigned int acknowledgementNumber;
-    // Contains the bits for Data Offset, Reserved Bits and the Control Bits
-    unsigned short infoBits;
-    unsigned short windowSize;
-    unsigned short checksum;
-    unsigned short urgentPointer;
-};
+
 
 struct TCP_Header_Fixed_Size {
     unsigned short sourcePort;
@@ -27,6 +17,16 @@ struct TCP_Header_Fixed_Size {
     unsigned short windowSize;
     unsigned short checksum;
     unsigned short urgentPointer;
+};
+
+struct TCP_Header {
+    TCP_Header_Fixed_Size headerFixedSize;
+    unsigned int options[10];
+};
+
+struct TCP_Packet {
+    TCP_Header tcpHeader;
+    char data[];
 };
 
 #endif //TCPPROTOCOL_TCP_PACKET_H
--- tcp/tcp_windows_connection.h
+++ tcp/tcp_windows_connection.h
@@ -10,83 +10,80 @@
 #include <ws2tcpip.h>
 #include <memory>
 #include <thread>
+#include <iostream>
+#include "tcp_packet.h"
 
 #define DEFAULT_INCOMING_DATA_BUFFER_SIZE 1024
 #define SUCCESS 0
-#define BASE_ERROR 1
+#define G_BASE_ERROR 1
+
+
 class TCP_Connection {
 private:
-    bool isBinded = false;
     SOCKET sock;
     std::thread thread;
+    bool keepListening = true;
 
-    [[nodiscard]] static int listenAndHandleConnections(SOCKET sock) {
+    int listenOn(void(*onPacketRecieved)(TCP_Packet tcpPacket, sockaddr_in clientInfo)) {
+        // Incoming connections will be routed to this socket for handling
         auto otherSocket = INVALID_SOCKET;
-        // TODO fill the last 2 arguements of accept with proper values: SO
+
+        // This struct is filled with client address information
         struct sockaddr_in clientSockAddrIn{};
         int addrLen = sizeof(clientSockAddrIn);
-        auto* ptr = reinterpret_cast<sockaddr *>(&clientSockAddrIn);
-        otherSocket = accept(sock, ptr, &addrLen);
-        if (otherSocket == INVALID_SOCKET) {
-            return BASE_ERROR;
-        }
-        char incomingDataBuffer[DEFAULT_INCOMING_DATA_BUFFER_SIZE];
-        int recvResults;
+        auto *ptr = reinterpret_cast<sockaddr *>(&clientSockAddrIn);
+
+        // listen for incoming connections
         do {
-            recvResults = recv(otherSocket, incomingDataBuffer, sizeof(incomingDataBuffer), 0);
-            //TODO do something with the data you received
-        } while(recvResults > 0);
-        if (recvResults == SOCKET_ERROR) {
-            // Error handle
-        }
+            // Accept a connection and send it to otherSocket for handling
+            otherSocket = accept(sock, ptr, &addrLen);
+            if (otherSocket == INVALID_SOCKET) {
+                //TODO potential error logging in the future
+                std::cout << "Socket failed to create" << "\n";
+                return G_BASE_ERROR;
+            }
+
+            // Buffer that will hold the data being recieved
+            char incomingDataBuffer[DEFAULT_INCOMING_DATA_BUFFER_SIZE];
+            int amountOfBytesReceived;
+
+            do {
+                amountOfBytesReceived = recv(otherSocket, incomingDataBuffer, sizeof(incomingDataBuffer), 0);
+
+                // Copy the first 20 bytes into the tcp fixed size header
+                struct TCP_Header_Fixed_Size fixedTCPHeader{};
+                memcpy(&fixedTCPHeader, incomingDataBuffer, sizeof(fixedTCPHeader));
+
+                // Create the full size tcp header
+                struct TCP_Header tcpHeader;
+                tcpHeader.headerFixedSize = fixedTCPHeader;
+                // Perform a bitwise and operator then shift it right 12 bits to get first 4 bits which is the Data Offset
+                short dataOffset = (tcpHeader.headerFixedSize.infoBits & 0xF000) >> 12;
+                memcpy(tcpHeader.options, incomingDataBuffer + 20, (dataOffset * 4) - 20);
+
+                // calculate the padding
+                int padding = (amountOfBytesReceived - sizeof tcpHeader) % 4;
+
+                // Construct tcp packet
+                struct TCP_Packet tcpPacket;
+                tcpPacket.tcpHeader = tcpHeader;
+                int offset = sizeof tcpPacket.tcpHeader + padding;
+                memcpy(tcpPacket.data, incomingDataBuffer + offset, amountOfBytesReceived - offset);
+
+                onPacketRecieved(tcpPacket, clientSockAddrIn);
+            } while (amountOfBytesReceived > 0 && keepListening);
+            // Log any errors
+            if (amountOfBytesReceived == SOCKET_ERROR) {
+                //TODO add error logging functionality in the future
+                return G_BASE_ERROR;
+            }
+        } while(keepListening);
 
         return SUCCESS;
     }
 
 
 public:
-
-    int bindTo(const int port) {
-
-        // Initialize the Winsock
-        WSADATA wsaData;
-        int wsaStartupResults = WSAStartup(MAKEWORD(2, 2), &wsaData);
-        if (wsaStartupResults >= WSABASEERR) {
-            // Log an error
-            // exit function
-            return BASE_ERROR;
-        }
-
-        sock = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
-        if (sock == INVALID_SOCKET) {
-            WSACleanup();
-            return BASE_ERROR;
-        }
-        struct sockaddr_in sockaddrIn{};
-        sockaddrIn.sin_family = AF_INET;
-        sockaddrIn.sin_port = htons(port);
-        sockaddrIn.sin_addr.s_addr = INADDR_ANY;
-
-        auto* ptr = reinterpret_cast<sockaddr *>(&sockaddrIn);
-
-        if (bind(sock, ptr, sizeof(sockaddrIn)) == SOCKET_ERROR) {
-            closesocket(sock);
-            WSACleanup();
-            return BASE_ERROR;
-        }
-
-        if (listen(sock, SOMAXCONN ) == SOCKET_ERROR) {
-            closesocket(sock);
-            WSACleanup();
-            return BASE_ERROR;
-        }
-
-        std::unique_ptr<std::thread> threadPtr;
-        threadPtr = std::make_unique<std::thread>(listenAndHandleConnections, sock);
-        thread.swap(*threadPtr);
-        isBinded = true;
-        return 0;
-    }
 
     int unbind() {
         int socketClosed = closesocket(sock);
@@ -96,6 +93,63 @@ public:
         isBinded = false;
         return 0;
     }
+
+    int send(const char (&buffer)[DEFAULT_INCOMING_DATA_BUFFER_SIZE], const char ip[4], const unsigned short port) {
+        // Initialize the Winsock
+        WSADATA wsaData;
+        int wsaStartupResults = WSAStartup(MAKEWORD(2, 2), &wsaData);
+        if (wsaStartupResults >= WSABASEERR) {
+            // Log an error
+            // exit function
+            return G_BASE_ERROR;
+        }
+
+    }
+
+    int listenOn(const std::string ip, const unsigned short port, void(*onPacketRecieved)(TCP_Packet tcpPacket, sockaddr_in clientInfo)) {
+        // Initialize the Winsock
+        WSADATA wsaData;
+        int wsaStartupResults = WSAStartup(MAKEWORD(2, 2), &wsaData);
+        if (wsaStartupResults >= WSABASEERR) {
+            // Log an error
+            // exit function
+            return G_BASE_ERROR;
+        }
+
+        sock = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
+        if (sock == INVALID_SOCKET) {
+            WSACleanup();
+            return G_BASE_ERROR;
+        }
+        // Construct ip from strig
+        struct sockaddr_in sockaddrIn{};
+        sockaddrIn.sin_family = AF_INET;
+        sockaddrIn.sin_port = htons(port);
+        if (inet_pton(AF_INET, ip.c_str(), &sockaddrIn.sin_addr) == -1) {
+            WSACleanup();
+            closesocket(sock);
+            return G_BASE_ERROR;
+        }
+
+        auto* ptr = reinterpret_cast<sockaddr *>(&sockaddrIn);
+
+        if (bind(sock, ptr, sizeof(sockaddrIn)) == SOCKET_ERROR) {
+            closesocket(sock);
+            WSACleanup();
+            return G_BASE_ERROR;
+        }
+
+        if (listen(sock, SOMAXCONN ) == SOCKET_ERROR) {
+            closesocket(sock);
+            WSACleanup();
+            return G_BASE_ERROR;
+        }
+
+        listenOn(onPacketRecieved);
+
+    }
+
+
 };
 
 #endif //TCPPROTOCOL_TCP_WINDOWS_CONNECTION_H
